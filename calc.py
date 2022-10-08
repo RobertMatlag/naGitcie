00f66d62562d1b3baba62d7e0b0341581b044fd5
Robert Matląg <r.matlag@getprintbox.com>
2022-10-08 16:37:02 +0200

    Init master

--- calc.py
+++ calc.py
@@ -6,14 +6,9 @@ def substract(a, b):
     pass
 
 
-def multiply(a: int, b: int) -> int:
-    return a * b
+def multiply(a, b):
+    pass
 
 
-def divide(a: float, b: float) -> float:
-    """
-    a: pierwsza liczba
-    b: druga liczba
-    return: wynik dzielenia
-    """
-    return a / b
+def divide(a, b):
+    pass
