cc30179357dcae06d14593525ce1b6b8cfe15b31
Robert Matląg <r.matlag@getprintbox.com>
2022-10-08 14:45:24 +0200

    Implement substracting

--- calc.py
+++ calc.py
@@ -1,9 +1,9 @@
-def add(a: int , b: int) -> int:
+def add(a: int, b: int) -> int:
     return a + b
 
 
-def substract(a, b):
-    pass
+def substract(a: int, b: int) -> int:
+    return a - b
 
 
 def multiply(a, b):
